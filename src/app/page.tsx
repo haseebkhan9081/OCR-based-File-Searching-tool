6ed86c34207281f101f2cd86ef6929bba7f5a337
haseebkhan9081 <aa9081haseeb@gmail.com>
2024-03-05 17:02:57 +0500

    first again

---
+++ src/app/page.tsx
@@ -0,0 +1,20 @@
+export default function Home() {
+  return (
+    <div
+    className="w-full
+    p-12
+    h-full
+    flex
+flex-col
+justify-center
+items-center
+    ">
+     <h1
+     className="text-5xl">Software engineering Project </h1>
+
+     <p> OCR managmenet tool or more like searching through the files tool
+     </p>
+   
+    </div>
+  );
+}
